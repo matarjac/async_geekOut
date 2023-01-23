8c11ce1f56cb3b58c5a0ad064ee07018779418ca
Matar Jacob <matar.j@outlook.com>
2023-01-23 22:22:17 +0200

    second commit

--- main.js
+++ main.js
@@ -50,42 +50,111 @@ data.then((resolve)=>console.log(resolve)).catch((error)=>console.log(error));
 
 // EX-2
 
-async function getAPI (){
-    try{
-        const res = await axios.get('https://restcountries.com/v2/all');
-        resData = res.data;
-        console.log(resData);
-    } catch{
-        console.log('err');
-    }
-    for(let i =0; i<100; i++){
+function renderCountries(data){
+    page.innerHTML='';
+    for(let index in data){
+        // create new div for the country
         newDiv = document.createElement('div');
         newDiv.className = 'country-div';
 
+        // create flag img element
         countryImg = document.createElement('img');
-        countryImg.src = resData[i].flag;
+        countryImg.src = data[index].flags.svg;
 
+        // create title for country name
         countryName = document.createElement('h3');
-        countryName.innerHTML = resData[i].name;
+        countryName.innerHTML = data[index].name.common;
 
+        // Create list of the country details
         countryDetails = document.createElement('ul');
-        countryDetails.innerHTML=`<li>Population: ${resData[i].population}</li> <li>Region: ${resData[i].region}</li> <li>Capital: ${resData[i].capital}`
+        countryDetails.innerHTML=`<li>Population: ${data[index].population}</li> <li>Region: ${data[index].region}</li> <li>Capital: ${data[index].capital}`
 
+        // Add all created elements to the parent div
         newDiv.appendChild(countryImg);
         newDiv.appendChild(countryName);
         newDiv.appendChild(countryDetails);
         page.appendChild(newDiv);
         
         
-        
+    }
+}
+
+async function getAPI (){
+    try{
+        const res = await axios.get('https://restcountries.com/v3.1/all');
+        console.log(res.data);
+        const data = res.data;
+        renderCountries(data);
+    } catch{
+        console.log('err');
     }
 
 }
-let page = document.getElementById('page')
-console.log(getAPI());
 
+async function getAPIByRegion(region){
+    console.log(region);
+    try{
+        const res = await axios.get(`https://restcountries.com/v3.1/region/${region}`);
+        console.log(res.data);
+        const data = res.data;
+        renderCountries(data);
+    } catch{
+        console.log('err');
+    }
+}
 
+async function getAPIBySearch(){
+    let name = inputSearchField.value;
+    console.log(name);
+    try{
+        const res = await axios.get(`https://restcountries.com/v3.1/name/${name}`);
+        console.log(res.data);
+        const data = res.data;
+        renderCountries(data);
+    } catch{
+        console.log('err');
+    }
+    console.log(data);
+}
 
+getAPI();
+
+const inputSearchField = document.querySelector('input');
+inputSearchField.addEventListener('input', ()=>{
+    getAPIBySearch();
+});
+
+let dropDownMenu = document.getElementById('menu');
+let dropDownMenuBtn = document.getElementById('drop-btn');
+dropDownMenuBtn.addEventListener('click', function(){
+    console.log('click');
+    dropDownMenu.classList.toggle('show');
+});
+
+let africaBtn = document.getElementById('africa');
+africaBtn.addEventListener('click', ()=>{
+    getAPIByRegion(africaBtn.id);
+});
+
+let americaBtn = document.getElementById('america');
+americaBtn.addEventListener('click', ()=>{
+    getAPIByRegion(americaBtn.id);
+});
+
+let asiaBtn = document.getElementById('asia');
+asiaBtn.addEventListener('click', ()=>{
+    getAPIByRegion(asia.id);
+});
+
+let europeBtn = document.getElementById('europe');
+europeBtn.addEventListener('click', ()=>{
+    getAPIByRegion(europeBtn.id);
+});
+
+let oceaniaBtn = document.getElementById('oceania');
+oceaniaBtn.addEventListener('click', ()=>{
+    getAPIByRegion(oceaniaBtn.id);
+});
 
 
 
